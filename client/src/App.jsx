eac4eea4ee40894b5e09e804bb31e6647171072d
Adityap340 <aditya_patil_comp@moderncoe.edu.in>
2024-02-19 12:14:36 +0530

    changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
 import Login from './pages/Login'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    <Navbar />
-    <Login />
+      <Navbar />
+      <Login />
     </>
   )
 }
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
+import IMG_6349 from '../assets/IMG_6349.png'
 
 const Navbar = () => {
   return (
-    
 
-<nav class="bg-white border-gray-200 dark:bg-pink-700">
-  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
-        <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
-        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">CodeFiesta</span>
-    </a>
-  </div>
-</nav>
+
+    <nav className="bg-white border-gray-200 dark:bg-pink-700">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <img src={IMG_6349} className="h-14" alt="Flowbite Logo" />
+          <span className="self-center text-5xl font-semibold whitespace-nowrap dark:text-white">CodeFiesta</span>
+        </a>
+      </div>
+    </nav>
   )
 }
 
